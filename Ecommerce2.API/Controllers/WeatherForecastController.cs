5002a9bb64ce8dbb5a82847023ec31acb852f126
ertgibson916 <ertgibson916@gmail.com>
2021-11-11 21:22:05 -0800

    Changed API model

--- Ecommerce2.API/Controllers/WeatherForecastController.cs
+++ Ecommerce2.API/Controllers/WeatherForecastController.cs
@@ -1,16 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
 
 namespace Ecommerce2.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -18,16 +17,12 @@ namespace Ecommerce2.API.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        [HttpPost(Name = "CreateUserInformation")]
+        public IActionResult Create(UserInformation userInformation) 
+        { 
+            //This code will save the UserInformation
         }
+
+         
     }
 }
--- Ecommerce2.API/WeatherForecast.cs
+++ Ecommerce2.API/WeatherForecast.cs
@@ -1,13 +1,17 @@
 namespace Ecommerce2.API
 {
-    public class WeatherForecast
+    public class UserInformation
     {
-        public DateTime Date { get; set; }
+        public string UserName { get; set; }
 
-        public int TemperatureC { get; set; }
+        public int Password { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public DateTime DOB { get; set; };
 
-        public string? Summary { get; set; }
+        public string? Description { get; set; }
+
+        public float Price { get; set; }
+
+        public string Title { get; set; }
     }
 }
